7304126da8cb7b67bd5e570d88dd9a3de0963f6c
joost00719 <joost1999@live.nl>
2017-06-25 12:59:31 +0200

    artikelen should work. binnekort even aan andere database linken want het lukte me niet om met mssql te verbinden.

--- phpfiles/includes/articles.php
+++ phpfiles/includes/articles.php
@@ -6,11 +6,41 @@ try {
         $artikel1 = "";
     }else{
         $artikel1 = $articleArray[0]['text'];
+        $kop1 = $articleArray[0]['h1'];
     }
-    $artikel2 = $articleArray[1]['text'];
-    $artikel3 = $articleArray[2]['text'];
-    $artikel4 = $articleArray[3]['text'];
-    $artikel5 = $articleArray[4]['text'];
+    ///////////////////////////////////////////////////////
+    if(empty($articleArray[1]['text'])){
+        $kop2 = "";
+        $artikel2 = "";
+    }else{
+        $artikel2 = $articleArray[1]['text'];
+        $kop2 = $articleArray[1]['h1'];
+    }
+    ///////////////////////////////////////////////////////
+    if(empty($articleArray[2]['text'])){
+        $kop3 = "";
+        $artikel3 = "";
+    }else{
+        $artikel3 = $articleArray[2]['text'];
+        $kop3 = $articleArray[2]['h1'];
+    }
+    ///////////////////////////////////////////////////////
+    if(empty($articleArray[3]['text'])){
+        $kop4 = "";
+        $artikel4 = "";
+    }else{
+        $artikel4 = $articleArray[3]['text'];
+        $kop4 = $articleArray[3]['h1'];
+    }
+    ///////////////////////////////////////////////////////
+    if(empty($articleArray[4]['text'])){
+        $kop5 = "";
+        $artikel5 = "";
+    }else{
+        $artikel5 = $articleArray[4]['text'];
+        $kop5 = $articleArray[4]['h1'];
+    }
+    ///////////////////////////////////////////////////////
 }catch(exception $e){
 }
 
@@ -19,11 +49,17 @@ try {
     <table>
         <tr>
             <td id="firstArticle">
+                <h1>
+                    <?=$kop1;?>
+                </h1>
                 <article id="a1">
                     <?=$artikel1;?>
                 </article>
             </td>
             <td>
+                <h1>
+                    <?=$kop2;?>
+                </h1>
                 <article id="a2">
                     <?=$artikel2;?>
                 </article>
@@ -33,16 +69,25 @@ try {
     <table>
         <tr>
             <td>
+                <h1>
+                    <?=$kop3;?>
+                </h1>
                 <article id="a3">
                     <?=$artikel3;?>
                 </article>
             </td>
             <td>
+                <h1>
+                    <?=$kop4;?>
+                </h1>
                 <article id="a4">
                     <?=$artikel4;?>
                 </article>
             </td>
             <td>
+                <h1>
+                    <?=$kop5;?>
+                </h1>
                 <article id="a5">
                     <?=$artikel5;?>
                 </article>
